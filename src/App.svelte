c8bf941ed5a3f6bed8c492114d28eb1d1c69d158
Rui Melo <rufimelo99@gmail.com>
2021-11-01 03:51:29 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,7 @@
+<div>
+	You have scrolled hii pixels
+</div>
+<!-- 
 <script>
 	const layers = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -86,3 +90,4 @@
 		background-color: rgb(253, 174, 51);
 	}
 </style>
+-->
