d97d1526e20685be4efd4b5286810db188ed5762
Rui Melo <rufimelo99@gmail.com>
2021-11-17 02:47:19 +0000

    Update ParallaxBackground.svelte

--- src/components/Parallax/ParallaxBackground.svelte
+++ src/components/Parallax/ParallaxBackground.svelte
@@ -28,7 +28,7 @@
 	<ParallaxLayer offset=0  rate=0.8 span=2>
 		<img src="assets/img/Parallax/p7.png" alt="" class="header" style="width: 100%;">
 	</ParallaxLayer>
-	
+	 
 	
 	<ParallaxLayer offset=0.65 rate=1.2 >
 			<img src="assets/img/hellothere.png" alt="" style="width: 100%;">
